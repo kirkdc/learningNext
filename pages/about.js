29e5049f1ce722c3bf3ec600fa1d60888abf955d
Kirk <kirk_dcosta@yahoo.com>
2019-07-15 16:52:32 +0530

    first commit, learning next project

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1> About BitzPrice </h1>
+      <p> This is an app that views BitzPrices </p>
+    </div>
+  </Layout>
+);
+
+export default About;
